512d1e095145277986d6d3b557cb6a4025af7803
minjai100111 <minjai100111@gmail.com>
2025-01-10 18:25:21 +0900

    edit dag

--- dags/jeju_car_sharing_company_locations.py
+++ dags/jeju_car_sharing_company_locations.py
@@ -20,6 +20,9 @@ os.environ["no_proxy"] = "*"
 SECRET_NAME = "ip-jeju-jeju-data-hub-key"
 REGION_NAME = "ap-northeast-2"
 
+BUCKET_RAWS = "ip-jeju-raws"
+BUCKET_TRANSFORMED = "ip-jeju-transformed"
+
 DATA_SET_ID = "3b93ba39-b550-477d-b13c-ed9044d335c7"  # QuickSight 데이터 세트 ID
 INGESTION_ID = "gas-price-refresh-ingestion"  # 고유 SPICE 새로고침 작업 ID
 GLUE_JOB_1 = "gas-price"
@@ -28,7 +31,7 @@ GLUE_JOB_2 = "gas-price-transform"
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "retries": 1,
+    "retries": 3,
     "retry_delay": timedelta(minutes=5),
 }
 
@@ -42,8 +45,8 @@ dag = DAG(
 
 with TaskGroup("bronze_layer", dag=dag) as bronze_layer:
 
-    def get_secret_key_from_secrets_manager(**kwargs):
-        ti = kwargs["ti"]
+    def get_secret_key_from_secrets_manager(**context):
+        secret_key = None
         try:
             client = boto3.client(
                 service_name="secretsmanager",
@@ -51,22 +54,20 @@ with TaskGroup("bronze_layer", dag=dag) as bronze_layer:
             )
             get_secret_value_response = client.get_secret_value(SecretId=SECRET_NAME)
             secret_key = json.loads(get_secret_value_response["SecretString"])["KEY"]
-            logging.info(f"ZZZZZZ {secret_key}")
+            logging.info(f"SECRET KEY: {secret_key}")
+            context["task_instance"].xcom_push(key="secret_key", value=secret_key)
         except ClientError as e:
             raise e
 
-        ti.xcom_push(key="secret_key", value=secret_key)
-
-    get_secret_key = PythonOperator(
-        task_id="get_secret_key",
+    get_secret_key_from_secrets_manager_task = PythonOperator(
+        task_id="get_secret_key_from_secrets_manager_task",
         python_callable=get_secret_key_from_secrets_manager,
         dag=dag,
     )
 
-    def get_data_from_api(**kwargs):
-        ti = kwargs["ti"]
-        secret_key = ti.xcom_pull(
-            task_ids="get_secret_key",
+    def get_data_from_api(**context):
+        secret_key = context["task_instance"].xcom_pull(
+            task_ids="bronze_layer.get_secret_key_from_secrets_manager_task",
             key="secret_key",
         )
         logging.info(secret_key)
@@ -83,21 +84,22 @@ with TaskGroup("bronze_layer", dag=dag) as bronze_layer:
 
             res_data = json.loads(res.content)["data"]
             logging.info(f"Data retrieved: {len(res_data)} records")
-            ti.xcom_push(key="res_data", value=res_data)
+            context["task_instance"].xcom_push(key="res_data", value=res_data)
         except requests.exceptions.RequestException as e:
             logging.error(f"API request failed: {e}")
             raise e
 
-    get_data = PythonOperator(
-        task_id="get_data",
+    get_data_from_api_task = PythonOperator(
+        task_id="get_data_from_api_task",
         python_callable=get_data_from_api,
+        retries=3,
+        retry_delay=timedelta(seconds=10),
         dag=dag,
     )
 
-    def prepare_csv_data(**kwargs):
-        ti = kwargs["ti"]
-        res_data = ti.xcom_pull(
-            task_ids="get_data_from_api",
+    def prepare_csv_data(**context):
+        res_data = context["task_instance"].xcom_pull(
+            task_ids="bronze_layer.get_data_from_api_task",
             key="res_data",
         )
         if not res_data:
@@ -107,31 +109,35 @@ with TaskGroup("bronze_layer", dag=dag) as bronze_layer:
         csv_data: str = df.to_csv(
             index=False, encoding="utf-8"
         )  # CSV 데이터를 문자열로 반환
-        return csv_data
+        context["task_instance"].xcom_push(key="csv_data", value=csv_data)
 
     prepare_csv_data_task = PythonOperator(
-        task_id="prepare_csv_data",
+        task_id="prepare_csv_data_task",
         python_callable=prepare_csv_data,
         dag=dag,
     )
 
     save_data_s3_raws_task = S3CreateObjectOperator(
-        task_id="save_data_s3_raws",
-        s3_bucket="ip-jeju-airflow-raws",  # TODO: 버킷 이름
+        task_id="save_data_s3_raws_task",
+        s3_bucket=BUCKET_RAWS,  # TODO: 버킷 이름
         s3_key="jeju_car_sharing_company_locations/jeju_car_sharing_company_locations.csv",  # TODO: S3 객체 키 (저장 경로 및 파일 이름)
-        data="{{ task_instance.xcom_pull(key='return_value', task_ids='prepare_csv_data') }}",
-        replace=False,
+        data="{{ task_instance.xcom_pull(key='csv_data', task_ids='bronze_layer.prepare_csv_data_task') }}",
+        replace=True,
         dag=dag,
     )
 
-    (get_secret_key >> get_data >> prepare_csv_data_task >> save_data_s3_raws_task)
+    (
+        get_secret_key_from_secrets_manager_task
+        >> get_data_from_api_task
+        >> prepare_csv_data_task
+        >> save_data_s3_raws_task
+    )
+
 
 with TaskGroup("silver_layer", dag=dag) as silver_layer:
 
-    def fetch_data_from_s3_raws(**kwargs):
-        ti = kwargs["ti"]
-        # S3 설정
-        s3_bucket = "ip-jeju-raws"
+    def fetch_data_from_s3_raws(**context):
+        s3_bucket = BUCKET_RAWS
         s3_key = (
             "jeju_car_sharing_company_locations/jeju_car_sharing_company_locations.csv"
         )
@@ -153,47 +159,44 @@ with TaskGroup("silver_layer", dag=dag) as silver_layer:
             logging.error(f"Failed to fetch data from S3: {e}")
             raise e
 
-        ti.xcom_push(key="fetched_data", value=df.to_dict(orient="records"))
+        context["task_instance"].xcom_push(
+            key="fetched_data", value=df.to_dict(orient="records")
+        )
 
     fetch_data_from_s3_raws_task = PythonOperator(
-        task_id="fetch_data_from_s3_raws",
+        task_id="fetch_data_from_s3_raws_task",
         python_callable=fetch_data_from_s3_raws,
         dag=dag,
     )
 
     # 1. Validate(유효성 검사)
-    def validate_data(**kwargs):
-        ti = kwargs["ti"]
-        data = ti.xcom_pull(
-            task_ids="fetch_data_from_s3_raws",
+    def validate_data(**context):
+        data = context["task_instance"].xcom_pull(
+            task_ids="silver_layer.fetch_data_from_s3_raws_task",
             key="fetched_data",
         )
         df = pd.DataFrame(data)
 
-        # 필수 컬럼 확인
-        required_columns = ["placeName", "category", "longitude", "latitude"]
-        for col in required_columns:
-            if col not in df.columns:
-                raise ValueError(f"Missing required column: {col}")
         # 데이터 타입 확인
         if not np.issubdtype(df["longitude"].dtype, np.number) or not np.issubdtype(
             df["latitude"].dtype, np.number
         ):
             raise ValueError("longitude and latitude must be numeric")
 
-        ti.xcom_push(key="validated_data", value=df.to_dict(orient="records"))
+        context["task_instance"].xcom_push(
+            key="validated_data", value=df.to_dict(orient="records")
+        )
 
     validate_data_task = PythonOperator(
-        task_id="validate_data",
+        task_id="validate_data_task",
         python_callable=validate_data,
         dag=dag,
     )
 
     # 2. Clean(데이터 정리)
-    def clean_data(**kwargs):
-        ti = kwargs["ti"]
-        data = ti.xcom_pull(
-            task_ids="validate_data_task",
+    def clean_data(**context):
+        data = context["task_instance"].xcom_pull(
+            task_ids="silver_layer.validate_data_task",
             key="validated_data",
         )
         df = pd.DataFrame(data)
@@ -203,35 +206,38 @@ with TaskGroup("silver_layer", dag=dag) as silver_layer:
         # 불필요한 공백 제거
         df["placeName"] = df["placeName"].str.strip()
 
-        ti.xcom_push(key="cleaned_data", value=df.to_dict(orient="records"))
+        context["task_instance"].xcom_push(
+            key="cleaned_data", value=df.to_dict(orient="records")
+        )
 
     clean_data_task = PythonOperator(
-        task_id="clean_data",
+        task_id="clean_data_task",
         python_callable=clean_data,
         dag=dag,
     )
 
     # 3. Standardize (표준화)
-    def standardize_data(**kwargs):
-        ti = kwargs["ti"]
-        data = ti.xcom_pull(
-            task_ids="clean_data_task",
+    def standardize_data(**context):
+        data = context["task_instance"].xcom_pull(
+            task_ids="silver_layer.clean_data_task",
             key="cleaned_data",
         )
         df = pd.DataFrame(data)
 
-        ti.xcom_push(key="standardized_data", value=df.to_dict(orient="records"))
+        context["task_instance"].xcom_push(
+            key="standardized_data",
+            value=df.to_dict(orient="records"),
+        )
 
     standardize_data_task = PythonOperator(
-        task_id="standardize_data",
+        task_id="standardize_data_task",
         python_callable=standardize_data,
         dag=dag,
     )
 
-    def save_parquet_to_s3(**kwargs):
-        ti = kwargs["ti"]
-        data = ti.xcom_pull(
-            task_ids="standardize_data_task",
+    def save_parquet_to_s3(**context):
+        data = context["task_instance"].xcom_pull(
+            task_ids="silver_layer.standardize_data_task",
             key="standardized_data",
         )
         if not data:
@@ -246,45 +252,47 @@ with TaskGroup("silver_layer", dag=dag) as silver_layer:
         buffer.seek(0)
 
         # S3에 업로드
-        s3_bucket = "ip-jeju-airflow-transformed"  # S3 버킷 이름
         s3_key = "jeju_car_sharing_company_locations/jeju_car_sharing_company_locations.parquet"  # 저장 경로 및 파일 이름
 
         try:
             s3_client = boto3.client("s3", region_name=REGION_NAME)
-            s3_client.upload_fileobj(buffer, s3_bucket, s3_key)
+            s3_client.upload_fileobj(buffer, BUCKET_TRANSFORMED, s3_key)
             logging.info(
-                f"Parquet file successfully uploaded to s3://{s3_bucket}/{s3_key}"
+                f"Parquet file successfully uploaded to s3://{BUCKET_TRANSFORMED}/{s3_key}"
             )
         except ClientError as e:
             logging.error(f"Failed to upload Parquet file to S3: {e}")
             raise e
 
     save_parquet_to_s3_task = PythonOperator(
-        task_id="save_parquet_to_s3",
+        task_id="save_parquet_to_s3_task",
         python_callable=save_parquet_to_s3,
         dag=dag,
     )
 
-    def create_glue_catalog(**kwargs):
+    def create_glue_catalog(**context):
         database_name = "ip_jeju_transformed_db"  # Glue Catalog 데이터베이스 이름
         table_name = "jeju_car_sharing_company_locations"  # Glue Catalog 테이블 이름
-        s3_bucket = "ip-jeju-airflow-transformed"
-        s3_key = (
-            "jeju_car_sharing_company_locations/transformed_data.parquet"  # S3 경로
-        )
+        s3_bucket = "ip-jeju-transformed"
+        s3_key = "jeju_car_sharing_company_locations/jeju_car_sharing_company_locations.parquet"  # S3 경로
 
         glue_client = boto3.client("glue", region_name=REGION_NAME)
 
         try:
-            # 데이터베이스 생성 (없을 경우)
             glue_client.create_database(
                 DatabaseInput={
                     "Name": database_name,
                     "Description": "Jeju car sharing locations database for ETL pipeline.",
                 }
             )
-            logging.info(f"Glue Database '{database_name}' created or already exists.")
+            logging.info(f"Database '{database_name}' created or already exists.")
+        except glue_client.exceptions.AlreadyExistsException:
+            logging.warning(f"Database '{database_name}' already exists.")
+        except Exception as e:
+            logging.error(f"Database creation failed: {e}")
+            raise e
 
+        try:
             # 테이블 생성
             glue_client.create_table(
                 DatabaseName=database_name,
@@ -313,18 +321,23 @@ with TaskGroup("silver_layer", dag=dag) as silver_layer:
                 },
             )
             logging.info(
-                f"Glue Table '{table_name}' created in database '{database_name}'."
+                f"Table '{table_name}' successfully created in '{database_name}'."
             )
         except glue_client.exceptions.AlreadyExistsException:
             logging.warning(
-                f"Glue Database or Table already exists: {database_name}.{table_name}"
+                f"Table '{table_name}' already exists in '{database_name}'."
             )
+            # 테이블이 존재하는 경우 세부 정보 로깅
+            existing_table = glue_client.get_table(
+                DatabaseName=database_name, Name=table_name
+            )
+            logging.info(f"Existing table details: {existing_table}")
         except Exception as e:
-            logging.error(f"Failed to create Glue Catalog: {e}")
+            logging.error(f"Table creation failed: {e}")
             raise e
 
     create_glue_catalog_task = PythonOperator(
-        task_id="create_glue_catalog",
+        task_id="create_glue_catalog_task",
         python_callable=create_glue_catalog,
         dag=dag,
     )
--- test.py
+++ test.py
@@ -1,13 +1,19 @@
-import json
+import io
 
 import boto3
+import pandas as pd
 
-client = boto3.client(
-    service_name="secretsmanager",
-    region_name="ap-northeast-2",
+s3_client = boto3.client("s3", region_name="ap-northeast-2")
+
+# S3 객체 다운로드
+response = s3_client.get_object(
+    Bucket="ip-jeju-raws",
+    Key="jeju_car_sharing_company_locations/jeju_car_sharing_company_locations.csv",
 )
-get_secret_value_response = client.get_secret_value(
-    SecretId="ip-jeju-jeju-data-hub-key"
-)
-secret = json.loads(get_secret_value_response["SecretString"])["KEY"]
-print(secret)
+print(f"Content Length: {response['ContentLength']} bytes")
+# print(response["Body"].read().decode("utf-8"))
+csv_content = response["Body"].read().decode("utf-8")  # CSV 데이터를 문자열로 읽기
+
+# CSV 데이터를 데이터프레임으로 변환
+df = pd.read_csv(io.StringIO(csv_content))
+print(f"Fetched {len(df)} rows from S3")
